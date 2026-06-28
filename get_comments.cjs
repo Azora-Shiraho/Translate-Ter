a25dbe49f33512018a569ce730bb670034132759
Azora-Shiraho <2056205354@qq.com>
2026-06-28 17:28:16 +0800

    chore: remove accidental scratch files from PR

--- get_comments.cjs
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const out = execSync('gh api repos/Azora-Shiraho/Translate-Ter/pulls/17/comments');
-const comments = JSON.parse(out.toString());
-let report = '';
-comments.forEach((c, i) => {
-  report += `\n--- Comment ${i + 1} ---\nFile: ${c.path}\nLine: ${c.line}\nBody:\n${c.body}\n`;
-});
-fs.writeFileSync('all_pr_comments.txt', report);
